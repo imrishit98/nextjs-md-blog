41667398996b84ffdd105a62e6f57bfac9a1d04a
imrishit98 <imrishit98@gmail.com>
2020-12-25 23:39:47 -0500

    init

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,13 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Image from 'next/image';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+import axios from 'axios';
 
-export default function Post({ postData }) {
+export default function Post({ postData, apiRes }) {
+  const testData = JSON.stringify(apiRes);
   return (
     <Layout>
       <Head>
@@ -15,25 +18,39 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
+        <code>{testData}</code> <br />
+        <a href={apiRes.DetailPageURL}>{apiRes.ASIN}</a> <br />
+        <Image
+          src={apiRes.Images.Primary.Large.URL}
+          alt='Picture of the product'
+          width={apiRes.Images.Primary.Large.Width}
+          height={apiRes.Images.Primary.Large.Height}
+        />
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
+  const resp1 = await axios.get(
+    `https://amazon-api-test.vercel.app/?prodId=${postData.test}`
+  );
+  const apiRes = await resp1.data;
+
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+      apiRes,
+    },
+  };
 }
